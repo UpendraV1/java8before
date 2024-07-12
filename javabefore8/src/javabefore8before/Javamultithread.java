f9cf27386d199da97aadbcd36791b16c7ce57bc4
upendra <luckyupendra1332@gmail.com>
2024-07-12 18:14:24 +0530

    newcommit

---
+++ javabefore8/src/javabefore8before/Javamultithread.java
@@ -0,0 +1,10 @@
+package javabefore8before;
+
+public class Javamultithread {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ javabefore8/src/module-info.java
@@ -0,0 +1,8 @@
+/**
+ * 
+ */
+/**
+ * 
+ */
+module javabefore8 {
+}
